7ea26b9902b58a0d3a96f8e0402d981e72108a2e
wqy55555 <906530937@qq.com>
2020-10-25 22:49:18 +0800

    提交信息

---
+++ FineAdmin.Repository/DBHeper/OracleHelper.cs
@@ -0,0 +1,16 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace FineAdmin.Repository
+{
+    public class OracleHelper
+    {
+        public static string oracleconnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["mysqlconn"].ConnectionString;
+
+        public static System.Data.IDbConnection GetConnection()
+        {
+            var connection = new MySqlConnection(oracleconnectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}
